7cae1ff927762e26e0e5dda458355e011d72f8ce
Renan Couto <renancouto@gmail.com>
2017-04-18 15:04:38 +0200

    Develop into Master (#1)
    * Add dependencies
    
    * Improve README
    
    * Fix package/repo names in package.json
    
    * Change severity level to string
    
    * Fix issues in the README
    
    * Add "License" section to README
    
    * Add publish configuration for scoped packages

---
+++ test/test.js
@@ -0,0 +1,33 @@
+/**
+ * dependencies
+ */
+const assert = require('assert');
+const config = require('../');
+
+/**
+ * isObject
+ * @param  {object}  obj target object
+ * @return {Boolean}     is object or not
+ */
+function isObject(obj) {
+    return typeof obj === 'object' && obj !== null;
+}
+
+/**
+ * tests
+ */
+describe('test basic properties of config', () => {
+    it('should be an object', () => {
+        assert(isObject(config));
+    });
+
+    it('should have the "rules" property', () => {
+        assert(config.rules);
+        assert(isObject(config.rules));
+    });
+
+    it('should extend "airbnb-base"', () => {
+        assert(config.extends);
+        assert(config.extends, 'airbnb-base');
+    });
+});
