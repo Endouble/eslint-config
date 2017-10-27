3c5f8c7650fb8afff915611680ea3c21bbbcf157
Renan Couto <renancouto@gmail.com>
2017-10-27 15:36:17 +0200

    Add tests for react configuration

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
  */
 const assert = require('assert');
 const config = require('../.');
+const react = require('../react');
 
 /**
  * isObject
@@ -16,18 +17,26 @@ function isObject(obj) {
 /**
  * tests
  */
-describe('test basic properties of config', () => {
+describe('test basic properties of all configurations', () => {
     it('should be an object', () => {
         assert(isObject(config));
+        assert(isObject(react));
     });
 
     it('should have the "rules" property', () => {
         assert(config.rules);
         assert(isObject(config.rules));
+        assert(react.rules);
+        assert(isObject(react.rules));
     });
 
     it('should extend "airbnb-base"', () => {
         assert(config.extends);
         assert(config.extends, 'airbnb-base');
+    });
+
+    it('should extend "airbnb"', () => {
+        assert(react.extends);
+        assert(react.extends, 'airbnb');
     });
 });
